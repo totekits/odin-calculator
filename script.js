386db5992a30354e43f5f402f0376a6d4e2fdd08
totekits <tote.kits@gmail.com>
2023-09-28 16:52:43 +0700

    Declare global consts and lets

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let num1 = 1;
-let num2 = 2;
-let opt = 'a';
+const display = document.getElementById('display');
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operators');
+const equalButton = document.getElementById('equal');
+const clearButton = document.getElementById('clear');
+const deleteButton = document.getElementById('delete');
+const periodButton = document.getElementById('period');
+
+let num1 = '';
+let num2 = '';
+let opt = '';
+let currentInput = 'num1';
 
 function add(num1, num2) {
     return num1 + num2; 
@@ -29,10 +38,11 @@ function operate(opt, num1, num2) {
     } else if (opt === 'd') {
         result = divide(num1, num2);
     } else {
-        console.log('ERROR')
+        console.log('')
     }
     return result;
 }
 
 const result = operate(opt, num1, num2);
+
 console.log(result);
